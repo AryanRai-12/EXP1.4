c2962914f172d55c381115d64f65acc8a02a5963
AryanRai-12 <122592507+AryanRai-12@users.noreply.github.com>
2024-02-08 12:32:14 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,8 @@
+# Simple-python
+def factorial(n):
+    return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+ 
+num = 5
+print("Factorial of",num,"is",factorial(num))
+print("Aryan Rai")
+print("UID: 21BCS11749")
